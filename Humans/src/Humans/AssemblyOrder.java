51bb6ba0108dd6ab8a7e5bbad60be48ead653fe9
1005307004 <1005307004@L829WD210013.edu.cbe.ab.ca>
2022-09-26 13:40:50 -0600

    i hate this

--- Humans/src/Humans/AssemblyOrder.java
+++ Humans/src/Humans/AssemblyOrder.java
@@ -1,10 +1,13 @@
 package Humans;
 
+
+import java.util.Arrays;
 import java.util.Comparator;
 
 public class AssemblyOrder implements Comparator<Human>  {
-	
-	 private int rankOfHuman(Human h) {
+
+
+	private static int rankOfHuman(Human h) {
 		int x = 0;
 		if(h.getClass() == WilliamAberhartStudent.class) {
 			x = 3;
@@ -15,20 +18,21 @@ public class AssemblyOrder implements Comparator<Human>  {
 		else if(h.getClass() == Adult.class) {
 			x = 1;
 		}
-		
-		return x;		 
-	 }
 
-	@Override
+		return x;		 
+	}
+
 	public int compare(Human o1, Human o2) {
-		if (rankOfHuman(o1) > rankOfHuman(o2)) {
+		Human[] names = {o1, o2};
+		Arrays.sort(names.length);
+		if (rankOfHuman(names[0]) > rankOfHuman(names[1])) {
 			return -1;
 		}
-		if (rankOfHuman(o1) < rankOfHuman(o2)) {
+		if (rankOfHuman(names[0]) < rankOfHuman(names[1])) {
 			return 1;
 		}
 		else {
-		return 0;
+			return 0;
 		}
 	}
 
--- Humans/src/Humans/NameOrder.java
+++ Humans/src/Humans/NameOrder.java
@@ -4,7 +4,6 @@ import java.util.Comparator;
 
 public class NameOrder implements Comparator<Human> {
 
-	@Override
 	public int compare(Human o1, Human o2) {
 		if(o1.getLastName().compareTo(o2.getLastName()) == 0) {
 			return o1.getFirstName().compareTo(o2.getFirstName());
